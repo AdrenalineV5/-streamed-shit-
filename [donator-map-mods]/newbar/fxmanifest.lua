d01be33ae8a448fe4bb79f3602fe8fff36d923cf
AdrenalineRPUK <adrenalineroleplaydrive@gmail.com>
2022-06-07 14:23:46 +0100

    1

---
+++ [donator-map-mods]/newbar/fxmanifest.lua
@@ -0,0 +1,28 @@
+lua54 'on'
+fx_version 'bodacious'
+game 'gta5'
+
+author 'junior99au'
+description 'The Emerald Bar @ Château damour'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/*.ytyp',
+    'stream/*.ymap',
+    'stream/*.ymf',
+    'stream/*.ytd',
+    'stream/*.ybn',
+    'stream/ss1_14_billboard.ydr',
+    'stream/ss1_14_chateau.ydr',
+    'stream/ss1_14_garage_01.ydr',
+    'stream/ss1_14_garagedecals.ydr',
+    'stream/ss1_14_ground.ydr',
+    'stream/ss1_14_ground2.ydr',
+    'stream/ss1_14_hotel01_emissive.ydr',
+    'stream/ss1_14_lod.ydr',
+    'stream/ss1_lod_06_06b_13_14_children.ydr',
+  }
+dependency '/assetpacks'
+dependency '/assetpacks'
