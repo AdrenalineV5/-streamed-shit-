21fe5e65cf5f7132e0b08e8a0037a802d4d9121b
AdrenalineRPUK <84265583+AdrenalineRPUK@users.noreply.github.com>
2022-06-22 17:27:04 +0100

    Buzgys car changes
    a

--- fetch.bat
+++ fetch.bat
@@ -1,4 +1,5 @@
 cd "C:\Users\DK Matt\Desktop\WIPE_SERVER\resources\[streamed-shit]"
 git fetch
+git pull
 git status
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Vehicles updated from github\"}" https://discord.com/api/webhooks/988747200811126834/gXIXOEX0Bz0EkjWhNsG7rH9GUtewOvupUysprxbN12I8ONe4Qz9XUVfOFizm27Pha2dA
